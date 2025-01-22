0a03086a98606d8c60f9bb9d8a2ee23773148d51
eeddy <eeddy@unb.ca>
2025-01-22 09:20:45 -0400

    Updated

--- src/components/ClassificationDatasets.tsx
+++ src/components/ClassificationDatasets.tsx
@@ -22,80 +22,80 @@ function ClassificationDatasets() {
     <div className="projects-container" id="classification">
         <h1>Classification Datasets</h1>
         <div className="projects-grid">
-            <div className="project" style={{display: 'flex', flexDirection: 'column'}}>
+            <div className="project">
                 <h2>DS1: General (3DC) &nbsp;<a href={`${window.location.origin}/3DC`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The 3DC dataset consists of 11 hand gestures performed by 22 participants and was recorded using the 3DC armband, which captured EMG signals from 10 channels at 1000 Hz. The dataset provides a within-session benchmark.</p>
+                <p style={{flex: 1, minHeight: '125px'}}>The 3DC dataset consists of 11 hand gestures performed by 22 participants and was recorded using the 3DC armband, which captured EMG signals from 10 channels at 1000 Hz. The dataset provides a within-session benchmark.</p>
                 <a href="https://www.mdpi.com/1424-8220/19/12/2811" target="_blank" rel="noreferrer"><img src={threedeec} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
-            <div className="project" style={{display: 'flex', flexDirection: 'column'}}>
-                <h2>DS2: Electrode Shift &nbsp;<a href={`${window.location.origin}/ElectrodeShift`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Electrode Shift dataset consists of data from 21 participants, recorded using the 8-channel Myo Armband at 200 Hz. The dataset provides a benchmark for evaluating myoelectric control robustness to electrode shift. </p>
-                <a href="https://link.springer.com/article/10.1186/s12984-024-01355-4" target="_blank" rel="noreferrer"><img src={es} className="zoom" alt="thumbnail" width="100%"/></a>
-            </div>
             <div className="project">
-                <h2>DS3: Minimal Training Data &nbsp;<a href={`${window.location.origin}/MinimalTrainingData`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Minimal Training Data dataset consists of data from 11 participants, recorded using the 8-channel Myo Armband at 200 Hz. The dataset provides a benchmark for evaluating model performance with limited training data. </p>
-                <a href="https://link.springer.com/article/10.1186/s12984-024-01355-4" target="_blank" rel="noreferrer"><img src={md} className="zoom" alt="thumbnail" width="100%"/></a>
-            </div>
-            <div className="project">
-                <h2>DS4: Weakly Supervised &nbsp;<a href={`${window.location.origin}/WeaklySupervised`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Weakly-Supervised dataset consists of data from 16 participants, including both screen-guided training and online user-in-the-loop control. The dataset includes 15 repetitions of 5 classes, aiming to support model development in user-in-the-loop scenarios.    </p>
-                <a href="https://ieeexplore.ieee.org/abstract/document/10802919" target="_blank" rel="noreferrer"><img src={weakly} className="zoom" alt="thumbnail" width="100%"/></a>
-            </div>
-            <div className="project">
-                <h2>DS5: Continuous Transitions &nbsp;<a href={`${window.location.origin}/ContinuousTransitions`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Continuous Transitions dataset consists of data from 43 participants, recorded using 6 bipolar Delsys electrodes at 2000 Hz. The dataset includes 7 gestures and evaluates a model's performance when there are continuous transitions between gestures.                </p>
+                <h2>DS2: Continuous Transitions &nbsp;<a href={`${window.location.origin}/ContinuousTransitions`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, minHeight: '125px'}}>The Continuous Transitions dataset consists of data from 43 participants, recorded using 6 Delsys electrodes at 2000 Hz. The dataset includes 7 gestures and evaluates a model's performance when there are continuous transitions between gestures.</p>
                 <a href="https://pubmed.ncbi.nlm.nih.gov/37721893/" target="_blank" rel="noreferrer"><img src={continuous} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS6: Contraction Intensity &nbsp;<a href={`${window.location.origin}/ContractionIntensity`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Contraction Intensity dataset consists of data from 10 participants, recorded using 8 electrodes sampling at 1000 Hz. The dataset includes 7 gestures performed at various force levels, evaluating a model's performance on contractions elicited at different intensities.                </p>
+                <h2>DS3: Contraction Intensity &nbsp;<a href={`${window.location.origin}/ContractionIntensity`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Contraction Intensity dataset consists of data from 10 participants, recorded using 8 electrodes sampling at 1000 Hz. The dataset includes 7 gestures performed at various force levels.</p>
                 <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC3719876/" target="_blank" rel="noreferrer"><img src={intensity} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS7: EMGEPN612 &nbsp;<a href={`${window.location.origin}/EMGEPN612`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The EMGEPN612 dataset consists of data from 612 users, each performing 6 gestures 50 times. The dataset was recorded using the 8-channel Myo Armband at 200 Hz and can be used for evaluating big-data cross-user and user-dependent models.                </p>
+                <h2>DS4: Electrode Shift &nbsp;<a href={`${window.location.origin}/ElectrodeShift`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Electrode Shift dataset consists of data from 21 participants, recorded using the 8-channel Myo Armband at 200 Hz. The dataset provides a benchmark for evaluating robustness to electrode shift.</p>
+                <a href="https://link.springer.com/article/10.1186/s12984-024-01355-4" target="_blank" rel="noreferrer"><img src={es} className="zoom" alt="thumbnail" width="100%"/></a>
+            </div>
+            <div className="project">
+                <h2>DS5: Cross-User (EMGEPN612) &nbsp;<a href={`${window.location.origin}/EMGEPN612`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The EMGEPN612 dataset consists of data from 612 users, each performing 6 gestures 50 times. The dataset was recorded using the 8-channel Myo Armband at 200 Hz and can be used for evaluating big-data cross-user and user-dependent models.                </p>
                 <a href="https://link-springer-com.proxy.hil.unb.ca/chapter/10.1007/978-3-030-86380-7_47" target="_blank" rel="noreferrer"><img src={epn} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS8: FORS-EMG &nbsp;<a href={`${window.location.origin}/FORSEMG`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The FORS-EMG dataset consists of data from 19 subjects, each performing 12 gestures at 3 forearm orientations. The dataset was recorded using a custom EMG device at 985 Hz, evaluating the influence of forearm orientation.                </p>
+                <h2>DS6: FORS-EMG &nbsp;<a href={`${window.location.origin}/FORSEMG`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The FORS-EMG dataset consists of data from 19 subjects, each performing 12 gestures at 3 forearm orientations. The dataset was recorded using a custom EMG device at 985 Hz, evaluating the influence of forearm orientation.                </p>
                 <a href="https://arxiv.org/abs/2409.07484" target="_blank" rel="noreferrer"><img src={fors} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS9: Limb Position (Fougner) &nbsp;<a href={`${window.location.origin}/LimbPosition`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Limb Position dataset consists of data from 12 participants, each performing 8 gestures in 5 limb positions. The dataset was recorded using 8 pairs of electrodes at 1000 Hz and can be used to evaluate the impact of limb position.                </p>
+                <h2>DS7: Limb Position (Fougner) &nbsp;<a href={`${window.location.origin}/LimbPosition`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Limb Position dataset consists of data from 12 participants, each performing 8 gestures in 5 limb positions. The dataset was recorded using 8 pairs of electrodes at 1000 Hz and can be used to evaluate the impact of limb position.                </p>
                 <a href="https://pubmed.ncbi.nlm.nih.gov/21846608/" target="_blank" rel="noreferrer"><img src={fougner} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS10: Cross-Session (GRABMyo) &nbsp;<a href={`${window.location.origin}/GRABMYO`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The GRABMyo dataset consists of data from 43 participants, each performing 17 gestures over 3 seperate sessions. The dataset was recorded using 16 electrodes sampling at 2048 Hz. This dataset can be used to evaluate cross-day resilience.                </p>
+                <h2>DS8: Cross-Session (GRABMyo) &nbsp;<a href={`${window.location.origin}/GRABMYO`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The GRABMyo dataset consists of data from 43 participants, each performing 17 gestures over 3 seperate sessions. The dataset was recorded using 16 electrodes sampling at 2048 Hz. This dataset can be used to evaluate cross-day resilience.                </p>
                 <a href="https://www.nature.com/articles/s41597-022-01836-y" target="_blank" rel="noreferrer"><img src={grab} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS11: High-Density (Hyser PR) &nbsp;<a href={`${window.location.origin}/Hyser`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Hyser PR dataset provides high-density EMG data for classifying 34 hand gestures, recorded from 10 participants using a 128-channel HD-EMG system.                </p>
+                <h2>DS9: High-Density (Hyser PR) &nbsp;<a href={`${window.location.origin}/Hyser`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Hyser PR dataset provides high-density EMG data for classifying 34 hand gestures, recorded from 10 participants using a 128-channel HD-EMG system. <br/><br/>           </p>
                 <a href="https://ieeexplore.ieee.org/document/9438637/" target="_blank" rel="noreferrer"><img src={hyserpr} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS12: Single-User Multi-Day &nbsp;<a href={`${window.location.origin}/MultiDay`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The KaufmannMD dataset consists of data from a single subject, recorded over 120 sessions, making it the largest cross-day EMG dataset from one subject. The dataset includes 11 motion classes, recorded using 4 channels at 2048 Hz, aiming to evaluate models' inter-session robustness.                </p>
+                <h2>DS10: Single-User Multi-Day &nbsp;<a href={`${window.location.origin}/MultiDay`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Minimal Data dataset consists of data from a single subject, recorded over 120 sessions. The dataset includes 11 motion classes, recorded using 4 channels at 2048 Hz, and aims to evaluate models' inter-session robustness.                </p>
                 <a href="https://pubmed.ncbi.nlm.nih.gov/21096692/" target="_blank" rel="noreferrer"><img src={kauffman} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS13: General (NinaPro DB2) &nbsp;<a href={`${window.location.origin}/NinaProDB2`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Ninapro DB2 dataset is a widely-used benchmark for myoelectric control algorithms, consisting of 50 gestures from 3 categories. The dataset was recorded from 12 channels at 2000 Hz and provides 4 training and 2 testing repetitions for each gesture.                </p>
+                <h2>DS11: Minimal Training Data &nbsp;<a href={`${window.location.origin}/MinimalTrainingData`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Minimal Training Data dataset consists of data from 11 participants, recorded using the 8-channel Myo Armband at 200 Hz. The dataset provides a benchmark for evaluating model performance with limited training data. </p>
+                <a href="https://link.springer.com/article/10.1186/s12984-024-01355-4" target="_blank" rel="noreferrer"><img src={md} className="zoom" alt="thumbnail" width="100%"/></a>
+            </div>
+            <div className="project">
+                <h2>DS12: General (NinaPro DB2) &nbsp;<a href={`${window.location.origin}/NinaProDB2`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Ninapro DB2 dataset is a widely-used benchmark for myoelectric control algorithms, consisting of 50 gestures from 3 categories. The dataset was recorded from 12 channels at 2000 Hz and provides 4 training and 2 testing repetitions for each gesture.                </p>
                 <a href="https://www.nature.com/articles/sdata201453" target="_blank" rel="noreferrer"><img src={db2} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS14: Limb Position (Radmand) &nbsp;<a href={`${window.location.origin}/LimbPosition`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Radmand Limb Position Dataset consists of data from 10 participants, each performing 8 gestures in 16 limb positions. The dataset was recorded using 6 Delsys Trigno electrodes at 1000 Hz, evaluating models' ability to generalize to unseen limb positions.                </p>
+                <h2>DS13: Limb Position (Radmand) &nbsp;<a href={`${window.location.origin}/LimbPosition`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Radmand Limb Position Dataset consists of data from 10 participants, each performing 8 gestures in 16 limb positions. The dataset was recorded using 6 Delsys Trigno electrodes at 1000 Hz, evaluating models' ability to generalize to unseen limb positions. <br/><br/>              </p>
                 <a href="https://pubmed.ncbi.nlm.nih.gov/25570046/" target="_blank" rel="noreferrer"><img src={radmand} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS15: Pre/Post TMR (TMRShirleyRyanAbilityLab) &nbsp;<a href={`${window.location.origin}/TMR`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The TMR dataset consists of data from 6 upper-limb amputee participants, before and after targeted muscle reinnervation surgery. Each participant performed 8 repetitions of 24 motion classes, including coarse wrist and fine finger movements. The dataset evaluates control benefits gained post-surgery.                </p>
+                <h2>DS14: Pre/Post TMR &nbsp;<a href={`${window.location.origin}/TMR`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The TMR dataset consists of data from 6 upper-limb amputee participants, before and after targeted muscle reinnervation surgery. Each participant performed 8 repetitions of 24 motion classes, including coarse wrist and fine finger movements. The dataset evaluates control benefits gained post-surgery.                </p>
                 <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC9879512/" target="_blank" rel="noreferrer"><img src={tmr} className="zoom" alt="thumbnail" width="100%"/></a>
+            </div>
+            <div className="project">
+                <h2>DS15: Weakly Supervised &nbsp;<a href={`${window.location.origin}/WeaklySupervised`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Weakly-Supervised dataset consists of data from 16 participants, including both screen-guided training and online user-in-the-loop control. The dataset includes 15 repetitions of 5 classes, aiming to support model development in user-in-the-loop scenarios.    </p>
+                <a href="https://ieeexplore.ieee.org/abstract/document/10802919" target="_blank" rel="noreferrer"><img src={weakly} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
         </div>
     </div>
--- src/components/RegressionDatasets.tsx
+++ src/components/RegressionDatasets.tsx
@@ -13,22 +13,22 @@ function RegressionDatasets() {
         <div className="projects-grid">
             <div className="project">
                 <h2>DS16: Cross-User (EMG2POSE) &nbsp;<a href={`${window.location.origin}/EMG2POSE`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The EMG2POSE dataset consists of data from 193 participants, each performing 29 distinct hand gestures and movements. The dataset includes 32 channels of EMG data and 40 joint angle labels, enabling the evaluation of within-subject and cross-subject pose estimation.                </p>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The EMG2POSE dataset consists of data from 193 participants, each performing 29 distinct hand gestures and movements. The dataset includes 32 channels of EMG data and 40 joint angle labels, enabling the evaluation of within-subject and cross-subject pose estimation.                </p>
                 <a href="https://arxiv.org/abs/2412.02725" target="_blank" rel="noreferrer"><img src={emg2pose} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
-                <h2>DS20: DS17-19: Hyser &nbsp;<a href={`${window.location.origin}/Hyser`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Hyser datasets consists of data from 20 participants, collected across two sessions, using a 256-channel high-density EMG system. The dataset includes three sub-datasets (1DOF, NDOF, Random) for evaluating regression models.                </p>
+                <h2>DS17-19: Hyser &nbsp;<a href={`${window.location.origin}/Hyser`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Hyser datasets consists of data from 20 participants, collected across two sessions, using a 256-channel high-density EMG system. The dataset includes three sub-datasets (1DOF, NDOF, Random) for evaluating regression models.<br/><br/>                </p>
                 <a href="https://ieeexplore.ieee.org/document/9438637/" target="_blank" rel="noreferrer"><img src={hyser} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
                 <h2>DS20: CyberGlove (Ninapro DB8)&nbsp;<a href={`${window.location.origin}/NinaProDB8`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The Ninapro DB8 dataset consists of data from 12 participants (10 able-bodied, 2 with limb difference), recorded using Delsys Trigno sensors and an 18-DoF dataglove. Participants performed 9 motions, with 10 repetitions for training and validation, and 2 repetitions for testing, facilitating exploration into continuous finger estimation using position labels.                </p>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The Ninapro DB8 dataset consists of data from 12 participants (10 able-bodied, 2 with limb difference), recorded using Delsys Trigno sensors and an 18-DoF dataglove. Participants performed 9 motions, with 10 repetitions for training and validation, and 2 repetitions for testing, facilitating exploration into continuous finger estimation using position labels.                </p>
                 <a href="https://www.frontiersin.org/journals/neuroscience/articles/10.3389/fnins.2019.00891/full" target="_blank" rel="noreferrer"><img src={db8} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
             <div className="project">
                 <h2>DS21: User Compliance &nbsp;<a href={`${window.location.origin}/UserCompliance`} target="_blank" rel="noopener noreferrer"><DataIcon /></a></h2>
-                <p style={{flex: 1, overflowY: 'auto', minHeight: '150px'}}>The User Compliance dataset consists of data from 6 participants, collecting 64-channel HD-EMG data from the forearm while performing 2 DoFs. The dataset explores user compliance with mimic training prompts, providing data for three emulated behaviors: baseline, anticipation, and all-or-nothing.                </p>
+                <p style={{flex: 1, overflowY: 'auto', minHeight: '125px'}}>The User Compliance dataset consists of data from 6 participants, collecting 64-channel HD-EMG data from the forearm while performing 2 DoFs. The dataset explores user compliance with mimic training prompts, providing data for three emulated behaviors: baseline, anticipation, and all-or-nothing. <br/><br/>                </p>
                 <a href="https://conferences.lib.unb.ca/index.php/mec/article/view/2507" target="_blank" rel="noreferrer"><img src={uc} className="zoom" alt="thumbnail" width="100%"/></a>
             </div>
         </div>
